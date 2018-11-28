4801c5c4f3878853fd20f80f9764e25b975fd170
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2018-11-28 23:59:05 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,10 +3,14 @@ const e = React.createElement;
 class Hello extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {date: new Date()};
   }
 
   render() {
-      return 'You can add, update and delete car information.';
+     return (
+     
+            " "+this.state.date.toLocaleDateString()+""
+        );
   }
 }
 
